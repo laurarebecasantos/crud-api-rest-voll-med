22dc792ee74154c27593eb338b522f2ac6af0e79
Laura <laurarebecasantos@gmail.com>
2024-08-20 08:14:14 -0300

    feat: added new models for Doctor and Address

---
+++ src/main/java/med/voll/api/model/Address.java
@@ -0,0 +1,21 @@
+package med.voll.api.model;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    private String street;
+    private String neighborhood;
+    private String zipCode;
+    private String city;
+    private String state;
+    private String complement;
+    private String number;
+}
