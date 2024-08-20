ae2524f22ec465d5cc008b8db79bb76aead22193
Laura <laurarebecasantos@gmail.com>
2024-08-20 00:00:38 -0300

    fix: rename class to follow camelCase convention

--- src/main/java/med/voll/api/doctor/medicalRegistrationData.java
+++
@@ -1,6 +0,0 @@
-package med.voll.api.doctor;
-
-import med.voll.api.address.AddressData;
-
-public record medicalRegistrationData(String name, String email, String crm, Speciality speciality, AddressData address ) {
-}
