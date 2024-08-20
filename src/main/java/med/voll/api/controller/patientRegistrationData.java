59b8be8da3fccc5905a01e858ee51ee29218c129
Laura Santos <45693034+laurarebecasantos@users.noreply.github.com>
2024-08-20 08:35:04 -0300

    Delete src/main/java/med/voll/api/controller/patientRegistrationData.java
    fix: removed obsolete class

--- src/main/java/med/voll/api/controller/patientRegistrationData.java
+++
@@ -1,6 +0,0 @@
-package med.voll.api.controller;
-
-import med.voll.api.address.AddressData;
-
-public record patientRegistrationData(String name, String email, String phone, String CPF, AddressData address) {
-}
