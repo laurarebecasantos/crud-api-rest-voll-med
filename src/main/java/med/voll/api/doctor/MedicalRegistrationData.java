1502ae09c8c3af8bf206793d963cfbcefd1936f0
Laura Santos <45693034+laurarebecasantos@users.noreply.github.com>
2024-08-20 08:36:40 -0300

    Delete src/main/java/med/voll/api/doctor directory
    fix: delete directory

--- src/main/java/med/voll/api/doctor/MedicalRegistrationData.java
+++
@@ -1,6 +0,0 @@
-package med.voll.api.doctor;
-
-import med.voll.api.address.AddressData;
-
-public record MedicalRegistrationData(String name, String email, String crm, Speciality speciality, AddressData address ) {
-}
