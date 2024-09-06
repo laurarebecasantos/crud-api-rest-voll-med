99c39f7ed8e3d988484c1598220505cb7af671c6
Laura <laurarebecasantos@gmail.com>
2024-09-06 17:43:26 -0300

    feat: add findAllByActiveTrue method to DoctorRepository

--- src/main/java/med/voll/api/repository/DoctorRepository.java
+++ src/main/java/med/voll/api/repository/DoctorRepository.java
@@ -3,5 +3,8 @@ package med.voll.api.repository;
 import med.voll.api.model.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+    Collection<Doctor> findAllByActiveTrue();
 }
